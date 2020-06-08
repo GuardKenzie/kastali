e1e89f99a68dce256da3b58e2109b8f2da26b35b
Tristan Ferrua <tristanferrua@gmail.com>
2020-06-08 00:10:03 +0000

    Kitty: Breytti stærð Neofetch: Setti rauða pöndu mynd, bætti veðri við nema betur með .dotfiles/vedur.sh, skipti "TH" út fyrir "CS" fyrir "colour scheme" Polybar: Bætti við bluetooth og breytti litum smá. Bluetooth er höndlað með .dotfiles/bt

--- .dotfiles/vedur.sh
+++ .dotfiles/vedur.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-echo "$(curl -s wttr.in/?0?q?T  | awk '/°(C|F)/ {printf $(NF-1) $(NF) " ("a")"} /,/ {a=$1}' | sed 's/\.\./-/g' | sed 's/,//g')" > $HOME/.dotfiles/vedur-nuna
+vedur="$(curl -s wttr.in/?0?q?T  | awk '/°(C|F)/ {printf $(NF-1) $(NF) " ("a")"} /,/ {a=$1}' | sed 's/\.\./-/g' | sed 's/,//g')"
+
+if [[ $vedur == '' ]]
+then
+    echo Could not get wttr.in > $HOME/.dotfiles/vedur-nuna
+else
+    echo $vedur > $HOME/.dotfiles/vedur-nuna
+fi
 
