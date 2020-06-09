1f81c30c0b5ddd026b4f2b0a8ca022088becc560
Tristan Ferrua <tristanferrua@gmail.com>
2020-06-09 22:54:58 +0000

    Betrumbætti veður script svo það reynir aftur ef engin tenging.
    Polybar nings

--- .dotfiles/vedur.sh
+++ .dotfiles/vedur.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-vedur="$(curl -s wttr.in/?0?q?T  | awk '/°(C|F)/ {printf $(NF-1) $(NF) " ("a")"} /,/ {a=$1}' | sed 's/\.\./-/g' | sed 's/,//g')"
 
-if [[ $vedur == '' ]]
-then
-    echo Could not get wttr.in > $HOME/.dotfiles/vedur-nuna
-else
-    echo $vedur > $HOME/.dotfiles/vedur-nuna
-fi
+stat=0
+count=0
+
+while [[ $stat == 0 && $count < 60 ]]
+do
+    vedur="$(curl -s wttr.in/?0?q?T  | awk '/°(C|F)/ {printf $(NF-1) $(NF) " ("a")"} /,/ {a=$1}' | sed 's/\.\./-/g' | sed 's/,//g')"
+    if [[ $vedur == '' ]]
+    then
+        echo Could not get wttr.in > $HOME/.dotfiles/vedur-nuna
+        sleep 1
+        count=$((count+1))
+    else
+        echo $vedur > $HOME/.dotfiles/vedur-nuna
+        stat=1
+    fi
+done
 
