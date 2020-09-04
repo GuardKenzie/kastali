2d0cc5eaf7878bf788ff978173d66a528c508b20
Tristan Ferrua <tristanferrua@gmail.com>
2020-09-04 15:44:58 +0000

    Fullt af dóti

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,7 @@ alias emt='emacsclient -t'
 
 # Commands redefinition
 alias clear='clear;neofetch'
+
+function icat
+    convert $argv[1] -resize 600x png:- | kitty +kitten icat --align left $argv[2..-1]
+end
